083daa937fddbb395da3034750a824a7d2eccc60
smilehyeon <ausp94@naver.com>
2021-09-27 23:44:01 +0900

    210927

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+function Food({fav}){
+  return <h1>I Like {fav}</h1>
+}
+
 function App() {
-  return <div>Hello!!!!</div>;
+  return (
+  <div>
+    <h1>Hello!!!!</h1>
+    <Food fav="kimchi" />
+    <Food fav="meat" />
+    <Food fav="coffee" />
+    <Food fav="cake" />
+  </div>);
 }
 
 export default App;
