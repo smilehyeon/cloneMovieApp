4f3b9c22ed53b8695100fdea070b349345dcd728
smilehyeon <ausp94@naver.com>
2022-10-11 21:28:02 +0900

    221011 라우터

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import React from "react";
-
-function Food({name, image}){
-  return <div>
-    <h2>I Like {name}</h2>
-    <img src = {image} />
-  </div>
-}
-
-const foodLike=[
-  {
-    name: "kimchi",
-    image: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https:%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F27617836561AE05F2A.jpg"
-  },
-  {
-    name: "meat",
-    image: "https://a57.foxnews.com/a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2018/09/640/320/1862/1048/steakpanistock.jpg?ve=1&tl=1?ve=1&tl=1.jpg"
-  },
-  {
-    name: "coffee",
-    image: "http://www.indigofinance.com.au/wp-content/uploads/2018/07/istock-157528129.jpg"
-  },
-  {
-    name: "cake",
-    image: "https://www.superiorwallpapers.com/download/carpet-made-of-coffee-beans-delicious-muffin-and-coffee-2560x1440.jpg"
-  }
-]
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return (
-  <div>
-    {foodLike.map(dish => <Food name={dish.name} image={dish.image} /> )}
-  </div>);
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </Router>;
 }
+
+
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ coverImg, title, summary }) {
+    return <div>
+        <img src={coverImg} alt={title} />
+        <h2>
+            <Link to="/movie">{title}</Link>
+        </h2>
+        <p>{summary}</p>
+    </div>
+}
+
+Movie.propTypes = {
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired
+}
+
+export default Movie; 
